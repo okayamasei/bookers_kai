14d8b91bac8bf39c4c59207788a8a216fdc6c2f2
岡山　靖 <okayamasei@seiokayama.local>
2020-04-15 14:25:59 +0900

    first

---
+++ db/migrate/20200415043737_add_karamu_to_books.rb
@@ -0,0 +1,7 @@
+class AddKaramuToBooks < ActiveRecord::Migration[5.2]
+  def change
+    add_column :books, :title, :string
+    add_column :books, :body, :text
+    add_column :books, :user_id, :integer
+  end
+end
